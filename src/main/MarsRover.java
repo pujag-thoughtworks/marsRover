938733676d642fc89dec01c2a4a34bc74b95a9ad
Puja Gupta <pujag@thoughtworks.com>
2017-06-05 16:52:22 +0530

    First commit

---
+++ src/main/MarsRover.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by pujag on 6/5/17.
+ */
+public class MarsRover {
+    private int xCoordinate;
+    private int yCoordinate;
+    private char orientation;
+    char[] currentOrientationAndGivenDirectionPair=new char[2];
+    HashMap<>
+    MarsRover(int xCoordinate,int yCoordinate, char orientation) {
+        this.xCoordinate=xCoordinate;
+        this.yCoordinate=yCoordinate;
+        this.orientation=orientation;
+    }
+
+
+    public char changeOrientation(char direction) {
+        if(orientation=='N');
+    }
+}
